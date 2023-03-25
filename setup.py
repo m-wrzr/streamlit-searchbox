fc1e5aca31fc16b3e95016cb783eca56077a4f89
Martin Wurzer <martin.wrzr@gmail.com>
2023-03-25 10:14:27 +0100

    bump version:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-searchbox",
-    version="0.1.0",
+    version="0.1.1",
     author="m-wrzr",
     description="Autocomplete Searchbox",
     long_description="Streamlit searchbox that dynamically updates "
