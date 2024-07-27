9a45632f62243ec22e9067a812adbad6093538bc
Martin Wurzer <martin.wrzr@gmail.com>
2024-07-27 14:48:11 +0200

    add version comments;

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setuptools.setup(
     classifiers=[],
     python_requires=">=3.8, !=3.9.7",
     install_requires=[
+        # version 1.37 reruns lead to constant iFrame resets
+        # version 1.35/1.36 also have reset issues but less frequent
         "streamlit >= 1.0, != 1.37.0",
     ],
     extras_require={
